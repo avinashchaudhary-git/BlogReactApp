5134c330591654e7bfaba8a81221f40e94736b9c
Aviansh7077 <avinashchaudhary1920@gmail.com>
2026-06-17 14:26:34 +0530

    initial build

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const AIBLog = () => {
+  return (
+    <>
+    <div>AIBLog</div>
+    {/* create a ordered list */}
+    <ol>
+      <li>RAG</li>
+      <li>LLM</li>
+      <li></li>
+    </ol>
+    </>
+  )
+}
+
+export default AIBLog
